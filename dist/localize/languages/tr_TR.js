f142413ca9b7d9d4c8b5fd7af8355e5b151130f3
Süleyman Akata <akata68@gmail.com>
2022-12-05 16:19:30 +0100

    Create tr_TR.js

---
+++ dist/localize/languages/tr_TR.js
@@ -0,0 +1,136 @@
+export const en = {
+    "common": {
+        "api_error": "API Hatasi",
+        "no_upcoming_games": "Yakında Maç yok %s",
+        "finalTerm": "%s - Maç Sonu",
+        "byeTerm": "BYE",
+        "tourney2": "Maç Sonu",
+        "tourney4": "yarı final",
+        "tourney8": "çeyrek final",
+        "tourney16": "Son 16",
+        "tourney32": "Son 32",
+        "tourney64": "Son 64",
+        "tourney128": "Erken Tur",
+        "tourney256": "Erken Tur"
+    },
+    "australian-football": {
+        "startTerm": "Başlama",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "kazanma olasılığı",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "baseball": {
+        "startTerm": "İlk Adım",
+        "overUnder": "O/U: %s",
+        "gameStat1": "Balls %s",
+        "gameStat2": "Strikes %s",
+        "gameStat3": "%s Outs",
+        "gameBar": "kazanma olasılığı",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "basketball": {
+        "startTerm": "Tipoff",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "kazanma olasılığı",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "football": {
+        "startTerm": "başlama vuruşu",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "kazanma olasılığı",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "golf": {
+        "startTerm": "Round Starts",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Shots (Thru)",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "hockey": {
+        "startTerm": "Puck Drop",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Shots on Goal",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "mma": {
+        "startTerm": "başlama vuruşu",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "kazanma olasılığı",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "racing": {
+        "startTerm": "Start",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Laps",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "soccer": {
+        "startTerm": "başlama vuruşu",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Şut (Hedefi tutan)",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "tennis": {
+        "startTerm": "First Serve",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "%s",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "volleyball": {
+        "startTerm": "First Serve",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "%s Score",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "default": {
+        "startTerm": "Start",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "kazanma olasılığı",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    }
+}
